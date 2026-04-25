156d9b73bd348075079926cc698559111088e494
smelki <saber.el.melki@gmail.com>
2026-04-25 11:00:39 +0100

    gitignore

--- crud/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connexion à la base de données
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=web2', 'root', '...');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Erreur de connexion : ' . $e->getMessage());
-}
-?>
--- tp_session_MVC/config/database.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-$pdo= new PDO('mysql:host=localhost;dbname=web2', 'root','...');
-//echo "connexion reussie <br>";
-
-?>
